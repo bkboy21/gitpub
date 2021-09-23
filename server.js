76d61b447489f87a1267139268717e2160bef6da
bkboy21 <89624517+bkboy21@users.noreply.github.com>
2021-09-23 10:16:28 -0400

    style added and drinks

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
-
+const drinks = require('./models/drinks.js')
 const port = 3000;
 
 app.get('//', (req, res) => {
     res.send('Welcome to the Gitpub App!');
+});
+
+
+app.get('/drinks/', (req, res) => {
+    res.render("drinks_index.ejs");
 });
 
 
