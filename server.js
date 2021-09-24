fca719673322765be6eaf2165b24a082dfbf7a0d
bkboy21 <89624517+bkboy21@users.noreply.github.com>
2021-09-23 20:30:09 -0400

    drink list rendering on ejs

--- server.js
+++ server.js
@@ -2,15 +2,23 @@ const express = require('express');
 const app = express();
 const drinks = require('./models/drinks.js')
 const port = 3000;
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.get('//', (req, res) => {
     res.send('Welcome to the Gitpub App!');
 });
 
 
-app.get('/drinks/', (req, res) => {
-    res.render("drinks_index.ejs");
+app.get('/drinks/:index/', (req, res)=>{
+    res.render("drinks_index.ejs", { allDrinks: drinks });
 });
+
+
+// app.get('/drinks/:index/', (req, res) => {
+//     let response = drinks[req.params.index];
+//     res.send(`<ul><li>${response.name}</li><li>${response.price}</li><li>${response.image}</li></ul>`);
+// });
 
 
 
